b3a7c413065651bad23caf5ae28bda5b7f401086
Théophane Durand <dev@theophanedurand.eu>
2021-09-11 22:57:44 +0200

    mise à jour

--- suivi.py
+++ suivi.py
@@ -7,25 +7,30 @@ FILE = 'signature_timing.csv'
 
 header = ['nb_new_signs', 'time']
 
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = csv.writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+
 r = requests.get(URL)
 count = 20
 i = 0
-file = open(FILE, 'w')
-with open(FILE, 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
+nb_sign = int(r.json()['total'])
+current_time = time.time()
+data = [nb_sign, current_time]
+append_list_as_row(FILE, data)
+old_nb_sign = nb_sign
+while(count > i):
+    r = requests.get(URL)
     nb_sign = int(r.json()['total'])
-    current_time = time.time()
-    data = [nb_sign, current_time]
-    writer.writerow(data)
-    old_nb_sign = nb_sign
-    while(count > i):
-        r = requests.get(URL)
-        nb_sign = int(r.json()['total'])
-        if(nb_sign > old_nb_sign):
-            current_time = time.time()
-            data = [[nb_sign - old_nb_sign, current_time]]
-            writer.writerow(data)
-            i += 1
-            old_nb_sign = nb_sign
-        time.sleep(1)
+    if(nb_sign > old_nb_sign):
+        current_time = time.time()
+        data = [nb_sign, current_time]
+        append_list_as_row(FILE, data)
+        i += 1
+        old_nb_sign = nb_sign
+    time.sleep(1)
